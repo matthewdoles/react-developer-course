f7c25e94584ad116288abafa758562b24cce6106
Matthew Doles <matteddied@gmail.com>
2019-12-11 14:52:28 -0500

    2.10 - Array Functions

---
+++ es6-concepts/array-functions.js
@@ -0,0 +1,12 @@
+const numbers = [1, 2, 3]
+
+const doubleNumArray = numbers.map((num) => {
+    return num * 2
+})
+console.log(numbers)
+// [1, 2, 3]
+console.log(doubleNumArray)
+// [2, 4, 6]
+
+// others inlcude: find, findIndex, filter, reduce,
+//                 concat, slice, splice
