f65a6b843f388e247cfb88294277753285963ae5
Matthew Doles <matteddied@gmail.com>
2019-12-12 15:57:42 -0500

    3.7 - Passing Method References Between Components

--- react-complete-guide/src/App.js
+++ react-complete-guide/src/App.js
@@ -11,12 +11,12 @@ const app = props => {
     ]
   });
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     setPersonsState({
       persons: [
         { name: 'Matt', age: 24 },
         { name: 'Maximilian', age: 29 },
-        { name: 'Steven', age: 26 }
+        { name: newName, age: 26 }
       ]
     })
   }
@@ -25,9 +25,9 @@ const app = props => {
     <div className="App">
       <h1>Hi, I'm a React App</h1>
       <p>This is really working!</p>
-      <button onClick={switchNameHandler}>Switch Names</button>
+      <button onClick={() => switchNameHandler('Kevin')}>Switch Names</button>
       <Person name={personsState.persons[0].name} age={personsState.persons[0].age} />
-      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} >My Hobbies: Teaching</Person>
+      <Person name={personsState.persons[1].name} age={personsState.persons[1].age} click={switchNameHandler.bind(this, 'Jeffy')}>My Hobbies: Teaching</Person>
       <Person name={personsState.persons[2].name} age={personsState.persons[2].age} />
     </div>
   );
