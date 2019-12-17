0050535d05e07012f40756b0d429599b3a129685
Matthew Doles <matteddied@gmail.com>
2019-12-17 15:54:19 -0500

    6.1 - Error Boundaries

--- react-complete-guide/src/Person/Person.js
+++ react-complete-guide/src/Person/Person.js
@@ -2,6 +2,9 @@ import React from 'react';
 import classes from './Person.css'
 
 const person = (props) => {
+    if (Math.random() > 0.7) {
+        throw new Error('Something went wrong')
+    }
     return (
         <div className={classes.Person}>
             <p onClick={props.click}>I'm {props.name} and I am {props.age} years old!</p>
