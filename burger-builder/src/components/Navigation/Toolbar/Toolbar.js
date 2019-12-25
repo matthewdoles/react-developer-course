30b48ec1e4e71c46c6baea0fd9cda86d22254fad
Matthew Doles <matteddied@gmail.com>
2019-12-25 13:51:07 -0500

    8.14 - Adding Navigation Items & Responsive Sidedrawer

--- burger-builder/src/components/Navigation/Toolbar/Toolbar.js
+++ burger-builder/src/components/Navigation/Toolbar/Toolbar.js
@@ -1,13 +1,18 @@
-import React from 'react'
-import classes from './Toolbar.css'
-import Logo from '../../Logo/Logo'
+import React from 'react';
+import classes from './Toolbar.css';
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems'
 
-const toolbar = () => (
+const toolbar = ( props ) => (
     <header className={classes.Toolbar}>
         <div>MENU</div>
-        <Logo />
-        <nav>...</nav>
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav>
+            <NavigationItems/>
+        </nav>
     </header>
-)
+);
 
-export default toolbar
+export default toolbar;
