0c961b7c8bc7dc8c361d9435dc3ce1c905c5fb82
Matthew Doles <matteddied@gmail.com>
2020-01-02 14:19:49 -0500

    11.5 - Extracting & Parsing Route Parameters

--- routing-react-concepts/src/containers/Blog/Blog.js
+++ routing-react-concepts/src/containers/Blog/Blog.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
-import { Route, Link } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 import Posts from './Posts/Posts'
 import NewPost from './NewPost/NewPost'
+import FullPost from './FullPost/FullPost'
 import './Blog.css';
 
 class Blog extends Component {
@@ -11,17 +12,25 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink 
+                                to="/" 
+                                exact
+                                activeClassName="my-active"
+                                activeStyle={{
+                                    color: '#FA923F',
+                                    textDecoration: 'underline'
+                            }}>Home</NavLink></li>
+                            <li><NavLink to={{
                                 pathname:'/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
                 <Route path="/" exact component={Posts} />
                 <Route path="/new-post" component={NewPost} />
+                <Route path="/:id" exact component={FullPost} />
             </div>
         );
     }
