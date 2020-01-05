2c62e1e54d14ece680a57f25c9eab887e36b7d24
Matthew Doles <matteddied@gmail.com>
2020-01-04 22:17:48 -0500

    12.4 - Passing Ingredients via Query Params & Contact Data Component

--- burger-builder/src/containers/Checkout/Checkout.js
+++ burger-builder/src/containers/Checkout/Checkout.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
+import { Route } from 'react-router-dom'
 import CheckoutSummary  from '../../components/Order/CheckoutSummary/CheckoutSummary'
+import ContactData from './ContactData/ContactData'
 
 class Checkout extends Component {
     state = {
@@ -9,6 +11,15 @@ class Checkout extends Component {
             cheese: 1,
             bacon: 1
         }
+    }
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search)
+        const ingredients = {}
+        for (let param of query.entries()) {
+            ingredients[param[0]] = +param[1]
+        }
+        this.setState({ ingredients })
     }
 
     checkoutCancelledHandler = () => {
@@ -26,6 +37,7 @@ class Checkout extends Component {
                     ingredients={this.state.ingredients}
                     checkoutCancelled={this.checkoutCancelledHandler}
                     checkoutContinued={this.checkoutContinuedHandler}/>
+                <Route path={this.props.match.path + '/contact-data'} component={ContactData} />
             </div>
         )
     }
