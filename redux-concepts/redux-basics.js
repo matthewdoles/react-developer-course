df695547d9ee9d611920da8a1e41a622d5614b16
Matthew Doles <matteddied@gmail.com>
2020-01-10 16:33:41 -0500

    14.2 - Dispatching Actions

--- redux-concepts/redux-basics.js
+++ redux-concepts/redux-basics.js
@@ -8,6 +8,18 @@ const initalstate = {
 
 // Reducer
 const rootReducer = (state = initalstate, action) => {
+    if  (action.type === 'INC_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + 1
+        }
+    }
+    if  (action.type === 'ADD_COUNTER') {
+        return {
+            ...state,
+            counter: state.counter + action.value
+        }
+    }
     return state
 }
 
@@ -16,6 +28,8 @@ const store = createStore(rootReducer)
 console.log(store.getState())
 
 // Action
-
+store.dispatch({type: 'INC_COUNTER'})
+store.dispatch({type: 'ADD_COUNTER', value: 10 })
+console.log(store.getState())
 
 // Subscription
