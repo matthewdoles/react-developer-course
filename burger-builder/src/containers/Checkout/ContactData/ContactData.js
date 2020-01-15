cabb6df39ab56ad4a33e5cabc322e204de43a8f1
Matthew Doles <matteddied@gmail.com>
2020-01-15 14:01:54 -0500

    17.5 - Order Reducer & Fixng Order Actions

--- burger-builder/src/containers/Checkout/ContactData/ContactData.js
+++ burger-builder/src/containers/Checkout/ContactData/ContactData.js
@@ -91,7 +91,6 @@ class ContactData extends Component {
             }
         },
         formIsValid: false,
-        loading: false
     }
 
     orderHandler = (event) => {
@@ -169,7 +168,7 @@ class ContactData extends Component {
                 <Button btnType="Success" clicked={this.orderHandler} disabled={!this.state.formIsValid}>ORDER</Button>
             </form>
         )
-        if (this.state.loading) {
+        if (this.props.loading) {
             form = <Spinner />
         }
         return (
@@ -184,13 +183,14 @@ class ContactData extends Component {
 const mapStateToProps = state => {
     return {
         ings: state.ingredients,
-        price: state.totalPrice
+        price: state.totalPrice,
+        loading: state.loading
     }
 }
 
 const mapDispatchToProps = dispatch => {
     return {
-        onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
+        onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
     }
 }
 
