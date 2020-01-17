45f40b68043bf0994780001376ce2bf00a3c475f
Matthew Doles <matteddied@gmail.com>
2020-01-17 13:58:08 -0500

    18.4 - Adding a Spinner & Loggin Users Out

--- burger-builder/src/containers/Auth/Auth.js
+++ burger-builder/src/containers/Auth/Auth.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { connect }  from 'react-redux'
 import Input from '../../components/UI/Input/Input'
 import Button from '../../components/UI/Button/Button'
+import Spinner from '../../components/UI/Spinner/Spinner'
 import classes from './Auth.css'
 import * as actions from '../../store/actions/'
 
@@ -103,7 +104,7 @@ class Auth extends Component {
             })
         }
 
-        const form = formElementArray.map(formElement => (
+        let form = formElementArray.map(formElement => (
             <Input 
                 key={formElement.id}
                 elementType={formElement.config.elementType}
@@ -115,8 +116,20 @@ class Auth extends Component {
                 touched={formElement.config.touched}/>
         ))
 
+        if (this.props.loading) {
+            form = <Spinner />
+        }
+
+        let errorMessage = null
+        if (this.props.error) {
+            errorMessage = (
+                <p>{this.props.error.message}</p>
+            )
+        }
+
         return (
             <div className={classes.Auth}>
+                {errorMessage}
                 <form onSubmit={this.submitHandler}>
                     {form}
                     <Button btnType="Success">SUBMIT</Button>
@@ -127,10 +140,17 @@ class Auth extends Component {
     }
 }
 
+const mapStateToProps = state => {
+    return {
+        loading: state.auth.loading,
+        error: state.auth.error
+    }
+}
+
 const mapDispatchToProps = dispatch => {
     return {
         onAuth: (email, password, isSignup) => dispatch(actions.auth(email, password, isSignup))
     }
 }
 
-export default connect(null, mapDispatchToProps)(Auth)
+export default connect(mapStateToProps, mapDispatchToProps)(Auth)
--- burger-builder/src/store/actions/auth.js
+++ burger-builder/src/store/actions/auth.js
@@ -22,6 +22,20 @@ export const authFail = (error) => {
     }
 }
 
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    }
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout())
+        }, expirationTime * 1000)
+    }
+}
+
 export const auth = (email, password, isSignup) => {
     return dispatch => {
         dispatch(authStart())
@@ -30,18 +44,19 @@ export const auth = (email, password, isSignup) => {
             password: password,
             returnSecureToken: true
         }
-        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=API_KEY'
+        let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=APIKEY'
         if (!isSignup) {
-            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=API_KEY'
+            url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=APIKEY'
         }
         axios.post(url, authData)
             .then(res => {
                 console.log(res)
                 dispatch(authSuccess(res.data.idToken, res.data.localId))
+                dispatch(checkAuthTimeout(res.data.expiresIn))
             })
             .catch(err => {
                 console.log(err)
-                dispatch(authFail())
+                dispatch(authFail(err.response.data.error))
             })
     }
 }
