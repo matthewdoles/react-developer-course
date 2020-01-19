3b02c18ad4b4e2b6584d4003be59da061a749a1b
Matthew Doles <matteddied@gmail.com>
2020-01-19 15:47:15 -0500

    19.2 - Using Env Variables & Removing console.logs()s

--- burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
+++ burger-builder/src/components/Burger/OrderSummary/OrderSummary.js
@@ -3,11 +3,6 @@ import Aux from '../../../hoc/Aux/Aux'
 import Button from '../../UI/Button/Button'
 
 class OrderSummary extends Component {
-    // Could be functional component, does not need to be class
-    componentWillUpdate() {
-        console.log('[OrderSummary] componentWillUpdate')
-    }
-
     render() {
         const ingredientSummary = Object.keys(this.props.ingredients)
         .map(igKey => {
