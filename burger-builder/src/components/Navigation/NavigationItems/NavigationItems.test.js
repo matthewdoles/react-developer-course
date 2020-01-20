b0062ee2828a2a2c92686e7a0503d8caa5713bd5
Matthew Doles <matteddied@gmail.com>
2020-01-20 11:29:17 -0500

    20.2 - Testing Containers

--- burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
+++ burger-builder/src/components/Navigation/NavigationItems/NavigationItems.test.js
@@ -21,4 +21,9 @@ describe('<NaivgationItems />', () => {
         wrapper.setProps({ isAuthenticated: true })
         expect(wrapper.find(NavigationItem)).toHaveLength(3)
     })
+
+    it('should render Logout <NavigationItem /> element if authenticated', () => {
+        wrapper.setProps({ isAuthenticated: true })
+        expect(wrapper.contains(<NavigationItem link="/logout">Logout</NavigationItem>)).toEqual(true)
+    })
 })
