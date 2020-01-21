cc5694237684259ee4ae1bcc5432bb77ef37b0b2
Matthew Doles <matteddied@gmail.com>
2020-01-21 15:43:13 -0500

    23.3 - Styling App in Next.js & Handling (404) Errors

---
+++ nextjs-react-concepts/pages/_error.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'next/link'
+import Router from 'next/router'
+
+const error = () => (
+    <div>
+        <h1>Oops, something went wrong.</h1>
+        <p>Try <Link href='/'><a>going back.</a></Link></p>
+    </div>
+)
+
+export default error
