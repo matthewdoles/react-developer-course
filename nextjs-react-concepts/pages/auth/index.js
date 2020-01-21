401b00870034518ca283a13d9d3d5e9427061174
Matthew Doles <matteddied@gmail.com>
2020-01-21 15:35:46 -0500

    23.2 - Next.js Basics & Components w/ Pages

--- nextjs-react-concepts/pages/auth/index.js
+++ nextjs-react-concepts/pages/auth/index.js
@@ -1,8 +1,10 @@
 import React from 'react'
+import User from '../../components/User'
 
 const authIndexPage = () => (
     <div>
         <h1>The Auth Page</h1>
+        <User name="Matthew" age={24} />
     </div>
 )
 
