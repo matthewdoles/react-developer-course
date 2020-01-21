5c9616730838f9b835f16a7c4acef5b83774c721
Matthew Doles <matteddied@gmail.com>
2020-01-21 17:34:34 -0500

    23.4 - Special Lifecycle Hook (getInitialProps)

--- nextjs-react-concepts/pages/auth/index.js
+++ nextjs-react-concepts/pages/auth/index.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import User from '../../components/User'
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
     <div>
-        <h1>The Auth Page</h1>
+        <h1>The Auth Page - {props.appName}</h1>
         <User name="Matthew" age={24} />
     </div>
 )
+
+authIndexPage.getInitialProps = (context) => {
+    const promise = new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve({ appName: 'Super Auth'})
+        }, 1000)
+    })
+    return promise
+}
 
 export default authIndexPage
