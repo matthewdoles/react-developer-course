14bb39729b87f5d27cbbfc49e38381572bd9edd4
Matthew Doles <matteddied@gmail.com>
2020-01-22 16:13:19 -0500

    24.4 - Using & Wrapping the Transition Component

--- animations-react/src/App.js
+++ animations-react/src/App.js
@@ -43,8 +43,8 @@ class App extends Component {
               }}
           />)}
         </Transition>
-        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
-        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
