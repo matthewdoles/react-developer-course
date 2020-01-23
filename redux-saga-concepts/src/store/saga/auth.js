9235fb78bbde73e848136910041217e06b1d3319
Matthew Doles <matteddied@gmail.com>
2020-01-23 15:10:48 -0500

    26.7 - Redux Saga Effects Dive

--- redux-saga-concepts/src/store/saga/auth.js
+++ redux-saga-concepts/src/store/saga/auth.js
@@ -1,12 +1,12 @@
 import { delay } from 'redux-saga/effects'
-import { put } from 'redux-saga/effects'
+import { put, call } from 'redux-saga/effects'
 import axios from 'axios'
 import * as actions from '../actions/'
 
 export function* logoutSaga(action) {
-    yield localStorage.removeItem('token');
-    yield localStorage.removeItem('expirationDate');
-    yield localStorage.removeItem('userId');
+    yield call([localStorage, 'removeItems'], 'token')
+    yield call([localStorage, 'removeItems'], 'expirationDate')
+    yield call([localStorage, 'removeItems'], 'userId')
     put(actions.logoutSucceed())
 }
 
