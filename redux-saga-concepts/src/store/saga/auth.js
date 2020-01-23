014cda3d18281bc18941a0a0d7339e1bac1c332e
Matthew Doles <matteddied@gmail.com>
2020-01-23 10:36:45 -0500

    26.3 - Moving Logic from Action Creators to a Saga

--- redux-saga-concepts/src/store/saga/auth.js
+++ redux-saga-concepts/src/store/saga/auth.js
@@ -1,11 +1,15 @@
+import { delay } from 'redux-saga/effects'
 import { put } from 'redux-saga/effects'
-import * as actionTypes from '../actions/actionTypes'
+import * as actions from '../actions/'
 
 export function* logoutSaga(action) {
     yield localStorage.removeItem('token');
     yield localStorage.removeItem('expirationDate');
     yield localStorage.removeItem('userId');
-    put({
-        type: actionTypes.AUTH_LOGOUT
-    })
+    put(actions.logoutSucceed())
+}
+
+export function* checkAuthTimeoutSaga(action) {
+    yield delay(action.expirationTime * 1000)
+    yield put(actions.logout())
 }
