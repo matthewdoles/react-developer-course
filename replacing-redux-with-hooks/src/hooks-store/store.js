7b8eaa3a0242cd921c2d2c491c2304480659286e
Matthew Doles <matteddied@gmail.com>
2020-01-30 17:55:43 -0500

    29.5 - Optimizing the Store Custom Hook

--- replacing-redux-with-hooks/src/hooks-store/store.js
+++ replacing-redux-with-hooks/src/hooks-store/store.js
@@ -4,7 +4,7 @@ let globalState = {};
 let listeners = [];
 let actions = {};
 
-export const useStore = () => {
+export const useStore = (shouldListen = true) => {
   const setState = useState(globalState)[1];
 
   const dispatch = (actionIdentifier, payload) => {
@@ -19,11 +19,16 @@ export const useStore = () => {
   listeners.push(setState);
 
   useEffect(() => {
-    listeners.push(setState);
+    if (shouldListen) {
+      listeners.push(setState);
+    }
+   
     return () => {
+      if (shouldListen) {
       listeners = listeners.filter(li => li !== setState);
+      }
     };
-  }, [setState]);
+  }, [setState, shouldListen]);
 
   return [globalState, dispatch];
 };
